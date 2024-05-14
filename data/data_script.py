e91a763ad2b7589b5faaa6f94ef1514cd7411ecb
NaikUrRehman <paknaik.101@gmail.com>
2024-05-14 15:17:01 +0500

    Add DVC setup files to Github

---
+++ data/data_script.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+from noaa_sdk import NOAA
+
+def get_data():
+    n = NOAA()
+    observations = n.get_observations('11365','US')
+    for observation in observations:
+        print(observation)
+        break
+
+    df = pd.DataFrame(observations)
+    df.to_csv(r"F:\University Material\Semester VIII\Mlops\CA7\data\data.csv", index=False)
+
+def dvc_push():
+    # Change directory to project root (where DVC is initialized)
+    os.chdir(r'F:\University Material\Semester VIII\Mlops\CA7')
+    # os.system('cd ..')
+    # Stage and commit new data with DVC
+    os.system('dvc add data/data.csv')
+    os.system('dvc commit -m "Push updated data.csv"')
+
+    # Push changes to remote storage (if configured)
+    os.system('dvc push')
+
+if __name__ == "__main__":
+    get_data()
+    dvc_push()
